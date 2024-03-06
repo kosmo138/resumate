520edad4fde8016b9efa001c9b241889ca992c3b
WGCAT <seed9878@naver.com>
2024-03-06 18:25:26 +0900

    Add: React App install

---
+++ client/app/letter/letterpage/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Button variant="contained">Hello World</Button>
+    </React.Fragment>
+  );
+}
+
+export default App;

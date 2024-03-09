8c6f28e8fc03148e949bb6f13f2a6846d966c3a8
suyons <github.choice044@passmail.com>
2024-03-10 06:44:00 +0900

    feat: shadcn/ui 컴포넌트 라이브러리 도입 등 (#8)
    2024-03-10 06:44
    - 다음 Docs를 참고하여 shadcn/ui 컴포넌트 라이브러리 도입
    https://ui.shadcn.com/docs/installation/next
    - 한글 폰트로 Noto Sans KR을 사용하도록 설정
    - Figma에서 shadcn/ui 컴포넌트를 이용한 목업 디자인 진행 예정
    https://www.figma.com/community/file/1203061493325953101

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,19 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        Hello from Next.js
-        <br />
-        This is index page!
-      </div>
-    </main>
+    <>
+      <main>
+        <div>
+          Hello from Next.js
+          <br />
+          This is index page!
+          <br />
+          한글 출력 테스트
+          <br />
+          <Button>로그인</Button>
+        </div>
+      </main>
+    </>
   );
 }

fe81cf49dcd9ceae030c8248e7037c98210491a8
suyons <github.choice044@passmail.com>
2024-03-18 14:43:45 +0900

    feat: 채용정보 레이아웃 코드 추가 (#24)
    2024-03-18 14:43

--- client/app/resume/[id]/layout.tsx
+++ client/app/resume/[id]/layout.tsx
@@ -6,7 +6,6 @@ export default function ResumeLayout() {
       <HeadingText subtext="자기소개서 생성에 필요한 최소의 정보를 입력해 주세요">
         이력서 수정
       </HeadingText>
-      <section>이력서 편집기가 여기에 옵니다.</section>
     </main>
   )
 }

979c1814ee205f988c0a5d743ac691c744908fcb
suyons <su02ga@outlook.com>
2024-03-19 17:14:42 +0900

    feat: 로그인 및 채용정보 프론트엔드 프로토타입 추가 등 (#24)
    2024-03-19 17:14
    - Navbar 컴포넌트에 로그인 및 회원정보 메뉴 버튼 추가
    - 채용정보에 예시 Input 추가
    - 기존의 이력서 및 자소서 페이지 코드 리팩토링

--- client/app/recruit/page.tsx
+++ client/app/recruit/page.tsx
@@ -1,12 +1,27 @@
 import HeadingText from "@/components/heading-text"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
 export default function RecruitMenu() {
   return (
-    <main className="container flex flex-col items-center py-8">
-      <HeadingText subtext="워크넷 제공 구인 공고를 확인할 수 있습니다">
+    <main className="container flex flex-col py-8">
+      <HeadingText subtext="워크넷 제공 구인 공고를 검색하실 수 있습니다">
         채용정보
       </HeadingText>
-      <div>채용공고 시작</div>
+      <section className="mt-6">
+        <div className="flex flex-row space-x-4 align-middle text-lg">
+          <label htmlFor="title" className="pt-1 font-bold">
+            제목
+          </label>
+          <Input
+            id="title"
+            type="text"
+            placeholder="제목을 입력해 주세요"
+            className="w-1/2 text-lg"
+          />
+          <Button className="align-middle">검색</Button>
+        </div>
+      </section>
     </main>
   )
 }

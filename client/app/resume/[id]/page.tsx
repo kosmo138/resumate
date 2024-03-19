6dc0f8b06a4533b0b417e01e613cb8b185df29b3
suyons <github.choice044@passmail.com>
2024-03-19 17:14:42 +0900

    feat: 로그인 및 채용정보 프론트엔드 프로토타입 추가 등 (#24)
    2024-03-19 17:14
    - Navbar 컴포넌트에 로그인 및 회원정보 메뉴 버튼 추가
    - 채용정보에 예시 Input 추가
    - 기존의 이력서 및 자소서 페이지 코드 리팩토링

--- client/app/resume/[id]/page.tsx
+++ client/app/resume/[id]/page.tsx
@@ -1,16 +1,20 @@
 import { baseUrl } from "@/config/metadata"
 import { ResumeBody } from "@/types/resume"
 import { notFound } from "next/navigation"
+import HeadingText from "@/components/heading-text"
 import ResumeForm from "@/components/resume/resumeform"
 
 export default async function ResumeEditor(id: string) {
-  const resumeId = parseInt(id)
-  const response = await fetch(`${baseUrl}/api/resume/${resumeId}`)
+  /* GET 요청에 대한 응답이 없으면 404 오류 페이지를 표시합니다 */
+  const response = await fetch(`${baseUrl}/api/resume/${id}`)
     .then((res) => res.json())
     .catch(() => notFound())
-  const resumeBody: ResumeBody = await response.json()
+  const resumeBody: ResumeBody = response.parse()
   return (
-    <main>
+    <main className="container flex flex-col items-center py-8">
+      <HeadingText subtext="이력서 생성에 필요한 최소의 정보를 입력해 주세요">
+        이력서 수정
+      </HeadingText>
       <ResumeForm resume={resumeBody} />
     </main>
   )

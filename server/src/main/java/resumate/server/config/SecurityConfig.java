de036627eced9c4c3060c8a8a73c2b8f73af53d1
suyons <github.choice044@passmail.com>
2024-03-21 15:53:52 +0900

    fix: POST 요청 시 403 Forbidden 오류 수정 (#30)
    2024-03-21 15:53
    - POST /api/login 요청 시 403 오류 발생 원인 파악
    - Spring Security에서 CSRF protection이 기본값으로 활성화되어 있어서 발생한 문제로 확인
    - csrf.ignoringRequestMatchers("/api/*") 구문 추가하여 보호를 비활성화

--- server/src/main/java/resumate/server/config/SecurityConfig.java
+++ server/src/main/java/resumate/server/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-/* package resumate.server.config;
+package resumate.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,16 +6,28 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+/**
+ * CSRF (Cross-Site Request Forgery) protection
+ * 기본값으로 활성화되어 있어 상태 변경이 가능한 HTTP 메서드에 대해 CSRF 토큰을 요구한다.
+ * GET은 문제 없으나 POST, PUT, DELETE 메소드에 대해 CSRF 토큰을 요구한다.
+ * 토큰이 없는 POST 요청에 대해서 403 Forbidden 에러를 반환한다.
+ * 따라서, "/api/*" 경로에 대한 요청에 대해서는 CSRF 보호를 비활성화한다.
+ * 
+ * @see https://velog.io/@woohobi/Spring-security-csrf란
+ * @see https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#disable-csrf
+ */
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests((auth) -> auth
-            .requestMatchers("/api/resume/**", "/api/letter/**").hasRole("USER")
-            .requestMatchers("/", "/api/login", "/index.html").permitAll()
-            .anyRequest().authenticated());
+                .csrf((csrf) -> csrf.ignoringRequestMatchers("/api/*"))
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/api/resume/*", "/api/letter/*").hasRole("USER")
+                        .requestMatchers("/", "/api/login", "/index.html").permitAll()
+                        .anyRequest().authenticated());
         return http.build();
     }
-} */
+}

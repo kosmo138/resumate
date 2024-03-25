ddb2aa545755eb7635479951a4277b214bce4ae6
yousunning <yousun_729@naver.com>
2024-03-25 12:17:10 +0900

    Fix: 이력서 목록 페이지 설정/자소서 버튼 수정

--- client/components/resume/resumecard.tsx
+++ client/components/resume/resumecard.tsx
@@ -2,6 +2,7 @@ import { Card, CardContent, CardFooter } from "@/components/ui/card"
 import Link from "next/link"
 import ResumeButton from "./resumebutton"
 import Image from "next/image"
+import { Input } from "@/components/ui/input"
 
 import {
   DropdownMenu,
@@ -27,39 +28,43 @@ export default function ResumeCard({
     <Card>
       <CardContent>
         <div className="flex flex-col p-5">
-          <DropdownMenu>
-            <DropdownMenuTrigger>
+          <div>
+            <DropdownMenu>
               {/* div 1 */}
-              <div className="ms-2 ">
-                <Image src="/set.svg" alt="바퀴" width={30} height={30} />
+
+              <div className="ms-2 flex justify-end">
+                <DropdownMenuTrigger>
+                  <Image src="/set.svg" alt="바퀴" width={30} height={30} />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <div className="font-bold">
+                    <DropdownMenuItem>제목변경</DropdownMenuItem>
+                    <DropdownMenuItem>복제하기</DropdownMenuItem>
+                    <DropdownMenuItem>다운로드</DropdownMenuItem>
+                    <div className="text-red-500">
+                      <DropdownMenuItem>삭제</DropdownMenuItem>
+                    </div>
+                  </div>
+                </DropdownMenuContent>
               </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <div className="font-bold">
-                <DropdownMenuItem>제목변경</DropdownMenuItem>
-                <DropdownMenuItem>복제하기</DropdownMenuItem>
-                <DropdownMenuItem>다운로드</DropdownMenuItem>
-                <div className="text-red-500">
-                  <DropdownMenuItem>삭제</DropdownMenuItem>
-                </div>
-              </div>
-            </DropdownMenuContent>
-          </DropdownMenu>
+            </DropdownMenu>
+          </div>
 
           {/* div 2 */}
-          <div className="mt-3 flex justify-between font-bold">
-            <input
+          <div className="mt-3 font-bold">
+            <Input
+              title="example-title"
               type="text"
               id="ResumeTitle"
               name="title"
               value={title}
-              className="w-full border border-gray-100 px-4 py-2 focus:border-blue-500 focus:outline-none"
+              placeholder="제목을 수정해주세요"
             />
           </div>
           {/* div 3 */}
           <div className="ml-2 mt-3">{updatedAt}</div>
           {/* div 4 */}
-          <div className="ml-20 mt-5 p-1">
+          <div className="mt-5 flex justify-end">
             <Link href="/letter">
               <ResumeButton />
             </Link>

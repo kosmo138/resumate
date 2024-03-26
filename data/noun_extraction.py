aa8947eaf2678765292ea864e6f24de8cd9bb15d
baeksona <100sona@naver.com>
2024-03-26 14:34:03 +0900

    Feat: 인재상 관련 단어 추출 기능 추가
    기업 이름 검색 시 그 기업의 인재상 사이트와 연관 높은 단어 추출
    
    Resolves: #35

--- data/noun_extraction.py
+++ data/noun_extraction.py
@@ -1,29 +1,33 @@
-# ======특정 사이트에 대한 html코드에서 명사만 추출하는 기능 코드=====================
 from bs4 import BeautifulSoup
 import requests
-from konlpy.tag import Komoran
+from konlpy.tag import Hannanum
 
 # 명사만 추출하는 함수 정의
 def extract_nouns_from_html(url):
     # 웹사이트의 HTML 코드 가져오기
-    html = requests.get(url).text
+    response = requests.get(url)
+    # 인코딩 설정
+    response.encoding = response.apparent_encoding
+    html = response.text
+
     # BeautifulSoup을 사용하여 HTML 파싱
     soup = BeautifulSoup(html, "html.parser")
+
     # HTML에서 텍스트 추출
     text = soup.get_text()
-    # Komoran 형태소 분석기 초기화
-    komoran = Komoran()
+    # Hannanum 형태소 분석기 초기화
+    hannanum = Hannanum()
     # 명사만 추출하여 리스트에 저장
-    nouns = komoran.nouns(text)
+    nouns = hannanum.nouns(text)
     return nouns
 
 # 특정 사이트 URL
-# url = "https://www.lgcns.com/careers/who-we-are/"   #오류
-# url = "https://www.samsungsvc.co.kr/intro/recruitment"  #ok
-# url = "https://www.samsungsds.com/kr/unsdgs/values.html" #오류
-# url = "https://www.lge.co.kr/company/recruit/talent" #오류
-# url = "https://www.sk.co.kr/ko/careers/person.jsp" #ok
-url = "https://www.skhynix.com/careers/UI-FR-CR01/" #ok
+# url = "https://www.lgcns.com/careers/who-we-are/"   
+# url = "https://www.samsungsvc.co.kr/intro/recruitment"  
+# url = "https://www.samsungsds.com/kr/unsdgs/values.html" 
+url = "https://www.lge.co.kr/company/recruit/talent" 
+# url = "https://www.sk.co.kr/ko/careers/person.jsp" 
+# url = "https://www.skhynix.com/careers/UI-FR-CR01/" 
 
 # 명사만 추출
 nouns = extract_nouns_from_html(url)

cd85a625c7dcdc9fd8335928966521c93ecac359
yousunning <yousun_729@naver.com>
2024-03-26 20:06:55 +0900

    Feat: 버튼기능 추가

--- client/components/resume/resumebutton.tsx
+++ client/components/resume/resumebutton.tsx
@@ -2,8 +2,16 @@ import React from "react"
 import { Button } from "@/components/ui/button"
 
 export default function ResumeButton() {
+  const handleClick = () => {
+    console.log("자기소개서 작성")
+  }
+
   return (
-    <Button variant="outline" className="ml-10 h-10 px-3 py-2">
+    <Button
+      onClick={handleClick}
+      variant="outline"
+      className="ml-10 h-10 px-3 py-2"
+    >
       자기소개서 작성
     </Button>
   )

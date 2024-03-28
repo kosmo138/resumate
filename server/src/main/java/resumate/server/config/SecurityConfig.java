3923acf7974559e0f8e84fe3d0ebbb752b07e9d5
suyons <github.choice044@passmail.com>
2024-03-28 16:45:50 +0900

    Fix: 이력서 조회 시 404 오류 수정 (#50)
    2024-03-28 16:45
    - ResumeController.java: GET /api/resume 요청 시 404 오류 발생 문제를 수정
    - pom.xml: Spring Security 의존성 제거
    - Java 주요 클래스의 각 메서드에 주석 추가
    - init.sql: 데이터베이스 초기화 / CRUD 쿼리 분류 및 주석 추가
    - index.html, ResumeControllerTest.java: 사용하지 않는 테스트 파일 삭제
    
    Fix #50

--- server/src/main/java/resumate/server/config/SecurityConfig.java
+++ server/src/main/java/resumate/server/config/SecurityConfig.java
@@ -2,13 +2,9 @@ package resumate.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
     /**
      * BCrypt
@@ -20,30 +16,5 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    /**
-     * CSRF (Cross-Site Request Forgery) protection
-     * 기본값으로 활성화되어 있어 상태 변경이 가능한 HTTP 메서드에 대해 CSRF 토큰을 요구한다.
-     * GET은 문제 없으나 POST, PUT, DELETE 메소드에 대해 CSRF 토큰을 요구한다.
-     * 토큰이 없는 POST 요청에 대해서 403 Forbidden 에러를 반환한다.
-     * 따라서, "/api/*" 경로에 대한 요청에 대해서는 CSRF 보호를 비활성화한다.
-     * 
-     * @see https://velog.io/@woohobi/Spring-security-csrf란
-     * @see https://docs.spring.io/spring-security/reference/servlet/exploits/csrf.html#disable-csrf
-     * @see https://hou27.tistory.com/entry/Spring-Boot-Spring-Security-적용하기-암호화
-     */
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .httpBasic((basic) -> basic.disable())
-                .csrf((csrf) -> csrf.ignoringRequestMatchers("/api/**"))
-                .sessionManagement((session) -> session.disable())
-                .formLogin((form) -> form.disable())
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/api/member", "/api/login", "/api/resume/**", "/api/letter/**", "/api/qna/**")
-                        .permitAll()
-                        .anyRequest().authenticated());
-        return http.build();
     }
 }
--- server/src/main/java/resumate/server/controller/MemberController.java
+++ server/src/main/java/resumate/server/controller/MemberController.java
@@ -20,22 +20,42 @@ import resumate.server.service.MemberService;
 public class MemberController {
     private final MemberService memberService;
 
+    /**
+     * POST /api/login -> 로그인
+     * 입력: {"email": "1@test.com", "password": "1111"}
+     * 출력: JWT 토큰 -> 쿠키에 저장
+     */
     @PostMapping(value = "/login", consumes = "application/json", produces = "application/json")
     public ResponseEntity<String> postLogin(@RequestBody Member member, HttpServletResponse response) {
         return memberService.loginHandler(member, response);
     }
 
+    /*
+     * POST /api/member -> 회원가입
+     * 입력: {"email": "1@test.com", "password": "1111"}
+     * 출력: 회원가입 성공 메시지
+     */
     @PostMapping(value = "/member", consumes = "application/json", produces = "application/json")
     public ResponseEntity<String> postMember(@RequestBody Member member) {
         return memberService.insertMember(member);
     }
 
+    /*
+     * PATCH /api/member -> 비밀번호 수정
+     * 입력: {"password": "1111", "password2": "2222"}
+     * 출력: 비밀번호 수정 성공 메시지
+     */
     @PatchMapping(value = "/member", consumes = "application/json", produces = "application/json")
     public ResponseEntity<String> patchMember(@RequestHeader("authorization") String bearer,
             @RequestBody Member member) {
         return memberService.updateMember(bearer, member);
     }
 
+    /*
+     * DELETE /api/member -> 회원탈퇴
+     * 입력: {"password": "2222"}
+     * 출력: 회원탈퇴 성공 메시지
+     */
     @DeleteMapping(value = "/member", consumes = "application/json", produces = "application/json")
     public ResponseEntity<String> deleteMember(@RequestHeader("authorization") String bearer,
             @RequestBody Member member) {

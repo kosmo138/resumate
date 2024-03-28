2e9274af84ce95eb7ee407f6dde6d16864f9e0ff
WGCAT <seed9878@naver.com>
2024-03-28 18:29:44 +0900

    Add: letter edit page select추가

--- client/app/letter/[id]/page.tsx
+++ client/app/letter/[id]/page.tsx
@@ -1,20 +1,25 @@
+"use client"
+import * as React from "react"
 import { baseUrl } from "@/config/metadata"
 import { LetterBody } from "@/types/letter"
 import { notFound } from "next/navigation"
 import HeadingText from "@/components/heading-text"
-import LetterForm from "@/components/letter/letterform"
-import SaveButton from "@/components/letter/button";
-import { LetterInputForm } from "@/components/letter/letter-form";
-import { DoubleForm } from "@/components/letter/double-form";
-import { LetterTextarea } from "@/components/letter/letter-textarea";
-import { LetterInputWideForm } from "@/components/letter/letter-wideform";
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Label } from "@/components/ui/label"
-import { DropdownMenu } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export default async function LetterEditor(id: string) {
+  const [position, setPosition] = React.useState("bottom")
   /* GET 요청에 대한 응답이 없으면 404 오류 페이지를 표시합니다 */
   // const response = await fetch(`${baseUrl}/api/letter/${id}`)
   //   .then((res) => res.json())
@@ -48,11 +53,24 @@ export default async function LetterEditor(id: string) {
 
       <div className="ml-20 my-3 w-full flex">
         <div className="flex items-center">
-          <Label className="text-1xl font-bold">카테고리</Label>
+          <Label className="text-1xl font-bold mr-3">카테고리</Label>
         </div>
-        <Input className="ml-2 w-1/5 mr-4" placeholder="드롭박스 미구현"></Input>
+        <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectItem value="성장과정">성장과정</SelectItem>
+          <SelectItem value="지원동기">지원동기</SelectItem>
+          <SelectItem value="성격의 장단점">성격의 장단점</SelectItem>
+          <SelectItem value="입사 후 포부">입사 후 포부</SelectItem>
+          <SelectItem value="기업 이해 및 분석">기업 이해 및 분석</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
         <div className="flex items-center">
-          <Label className="text-1xl font-bold">키워드</Label>
+          <Label className="ml-3 text-1xl font-bold">키워드</Label>
         </div>
         <Input className="ml-2 w-1/5" placeholder="직무명 입력"></Input>
       </div>
@@ -73,10 +91,10 @@ export default async function LetterEditor(id: string) {
       </div>
       <div className="ml-20 w-full">
         <div>
-          <Button className="mr-2 bg-white text-black border-black">삭제</Button>
+          <Button variant="outline" className="mr-2">삭제</Button>
           <Button>추가</Button>
         </div>
-          <Button className="text-black border-black bg-white">생성하기</Button>
+          <Button variant="outline" className="text-black border-black bg-white">생성하기</Button>
       </div>  
 
     </main>

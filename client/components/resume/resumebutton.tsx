975e17396873bad5443c9d942d07aa0e9a8bf117
yousunning <yousun_729@naver.com>
2024-03-29 19:28:45 +0900

    Feat: 이력서 GET/DELETE  기능 및 ResumeAddButton 위치 이동

--- client/components/resume/resumebutton.tsx
+++ client/components/resume/resumebutton.tsx
@@ -2,8 +2,39 @@ import React from "react"
 import { Button } from "@/components/ui/button"
 
 export default function ResumeButton() {
+  const url = "http://localhost/api/resume/"
+
   const handleClick = () => {
-    console.log("자기소개서 작성")
+    //POST할 데이터
+    const data = {
+      title: "",
+      carrerData: [{ data: "", content: "" }],
+      carrerText: "",
+      education: [{ data: "", content: "" }],
+      skill: "",
+      award: [{ data: "", content: "" }],
+      language: "",
+    }
+    //fetch를 사용하여 post 요청 보내기
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok")
+        }
+        return response.json() //응답 JSON파싱
+      })
+      .then((json) => {
+        console.log("POST 요청 성공:", json)
+      })
+      .catch((error) => {
+        console.log("POST 요청실패", error)
+      })
   }
 
   return (

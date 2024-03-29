19858f60a210c17df614efd15cc6a66866e71c54
baeksona <100sona@naver.com>
2024-03-29 19:34:31 +0900

    Test: SSL 인증 확인 과정 무시, cp949 디코딩 방식 시도
    - cp949디코딩 방식 시도이유 : 특정 사이트에서 명사 추출 불가 문제.

--- data/app/api/main.py
+++ data/app/api/main.py
@@ -4,9 +4,3 @@ from app.api.routes import keyword
 
 api_router = APIRouter()
 api_router.include_router(keyword.router, prefix="/keyword", tags=["keyword"])
-from fastapi import APIRouter
-
-from api.routes import keyword
-
-api_router = APIRouter()
-api_router.include_router(keyword.router, prefix="/keyword", tags=["keyword"])

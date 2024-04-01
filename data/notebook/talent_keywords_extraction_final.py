9bcabbd9784d2b5f8981cc75cb465960510e0685
baeksona <100sona@naver.com>
2024-04-01 13:42:25 +0900

    Fix: 범용성 확장한 인재상 키워드 추출 기능
    - talent_keywords_extraction_final: options 오류 해결 후 동적인 텍스트까지도 추출 가능.
    
    Resolves: #47

--- data/notebook/talent_keywords_extraction_final.py
+++ data/notebook/talent_keywords_extraction_final.py
@@ -1,46 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+from konlpy.tag import Kkma
+from gensim.models import Word2Vec
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
-from konlpy.tag import Kkma
-from gensim.models import Word2Vec
-import pandas as pd
-from selenium.common.exceptions import TimeoutException
-from urllib.request import urlopen
-import ssl
-import requests
-from bs4 import BeautifulSoup
-from konlpy.tag import Kkma
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+import time
 
-class GoogleSearchScraper:
-    def __init__(self, options):
-        self.chrome_options = options
-
-    def get_first_search_result_url(self, query):
-        print("크롬 웹 드라이버를 실행합니다...")
-        chrome_driver_path = "chromedriver.exe"  # 직접 설치된 드라이버의 경로로 지정
-
-        print("구글 검색 페이지에 접근합니다...")
-        driver = webdriver.Chrome(options=self.chrome_options)  # 수정된 부분
-        driver.get(f"https://www.google.com/search?q={query}" + " 인재상")
-        try:
-            print("첫 번째 항목의 URL을 가져오는 중...")
-            first_title = WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, "h3.LC20lb.MBeuO.DKV0Md"))
-            )
-            url = first_title.find_element(By.XPATH, "./parent::a").get_attribute("href")
-            print("첫 번째 검색 결과 URL을 성공적으로 가져왔습니다.")
-            print("첫 번째 검색 결과 URL:", url)
-            return url
-        except TimeoutException:
-            print("첫 번째 검색 결과 URL을 가져오는 동안 시간이 초과되었습니다.")
-            return None
-        finally:
-            driver.quit()
-
+# HTML에서 텍스트 추출 및 명사 추출
 class TextAnalyzer:
     def __init__(self):
         self.kkma = Kkma()
@@ -48,14 +18,11 @@ class TextAnalyzer:
     def extract_nouns_from_html(self, url):
         try:
             print("URL에서 HTML 내용을 가져옵니다...")
-            # SSL 인증서 확인 과정을 무시하고 데이터를 가져옵니다.
-            response = urlopen(url)
-            # html = response.read().decode('utf-8')
-            html = response.read()
-            html_decoded = html.decode('cp949', 'ignore')
+            response = requests.get(url, verify=False)
+            html_text = response.text
             print("HTML 내용을 성공적으로 가져왔습니다.")
             print("HTML을 파싱합니다...")
-            soup = BeautifulSoup(html_decoded, "html.parser")
+            soup = BeautifulSoup(html_text, "html.parser")
             text = soup.get_text()
             print("HTML을 성공적으로 파싱했습니다.")
             print("명사를 추출합니다...")
@@ -66,6 +33,7 @@ class TextAnalyzer:
             print(f"명사를 추출하는 데 실패했습니다: {e}")
             return None
 
+# Word2Vec 모델 로드 및 관련 키워드 추출
 class KeywordExtractor:
     def __init__(self, model_path):
         self.model_path = model_path
@@ -93,6 +61,51 @@ class KeywordExtractor:
                     pass
         return related_keywords
 
+# Google 검색을 통해 첫 번째 검색 결과 URL 획득
+class GoogleSearchScraper:
+    def __init__(self, options):
+        self.chrome_options = options
+
+    def scroll_down_page(self, driver):
+        SCROLL_PAUSE_TIME = 2
+
+        # Get scroll height
+        last_height = driver.execute_script("return document.body.scrollHeight")
+
+        while True:
+            # Scroll down to bottom
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # Wait to load page
+            time.sleep(SCROLL_PAUSE_TIME)
+
+            # Calculate new scroll height and compare with last scroll height
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+    def get_first_search_result_url(self, query):
+        try:
+            print("크롬 웹 드라이버를 실행합니다...")
+            driver = webdriver.Chrome(options=self.chrome_options)
+            print("구글 검색 페이지에 접근합니다...")
+            driver.get(f"https://www.google.com/search?q={query}" + " 인재상")
+            self.scroll_down_page(driver)  # 스크롤을 내립니다.
+            print("첫 번째 항목의 URL을 가져오는 중...")
+            first_title = WebDriverWait(driver, 20).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "h3.LC20lb.MBeuO.DKV0Md"))
+            )
+            url = first_title.find_element(By.XPATH, "./parent::a").get_attribute("href")
+            print("첫 번째 검색 결과 URL을 성공적으로 가져왔습니다.")
+            print("첫 번째 검색 결과 URL:", url)
+            return url
+        except Exception as e:
+            print(f"검색 결과 URL을 가져오는 중 오류가 발생했습니다: {e}")
+            return None
+        finally:
+            driver.quit()
+
 def main():
     query = input("검색어를 입력하세요: ")
 
@@ -113,7 +126,7 @@ def main():
 
             extractor = KeywordExtractor(model_path="wiki.model")
             if extractor.model:
-                target_words = ["인재", "소통", "능력", "책임", "성실", "도전", "열정", "꿈", "사람"]
+                target_words = ["인재", "소통", "능력", "책임", "성실", "도전", "열정", "꿈", "사람", "다양", "창의", "혁신", "전문", "기술", "지식", "해결", "리더십", "팀워크", "목표", "정직", "도덕", "학습", "성장", "비전"]
                 related_keywords = extractor.extract_related_keywords(nouns, target_words)
 
                 print("인재상에 해당하는 키워드:", len(related_keywords), "개")

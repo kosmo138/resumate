1063e4be9b077d5b409e49f12a76e9f96ed7f8c8
suyons <github.choice044@passmail.com>
2024-04-02 23:05:49 +0900

    Refactor: 키워드 추출 모듈 분리 및 Docker 환경 실행 준비 (#56)
    2024-04-02 23:05
    - 키워드 서비스의 구성 요소를 3가지로 분리: 스크래퍼, 추출기, 서비스
    - 현재 아래의 내용대로 CSS 선택자에 접근할 수 없는 오류가 발생 중
    - 'WebDriver' object has no attribute 'find_element_by_css_selector'

---
+++ data/app/service/keywordservice.py
@@ -0,0 +1,79 @@
+import json
+import concurrent.futures
+from fastapi import HTTPException
+from app.core.models import Keyword
+from app.core.database import session
+from app.service.textscraper import TextScraper
+from app.service.keywordextractor import KeywordExtractor
+
+
+# 회사명 및 인재상 키워드 CRUD on MySQL
+def insert_keyword(company, keyword_list):
+    keyword_json = json.dumps(keyword_list)
+    new_keyword = Keyword(company=company, keyword=keyword_json)
+    session.add(new_keyword)
+    session.commit()
+
+
+def select_keyword(company):
+    record = session.query(Keyword).filter(Keyword.company == company).first()
+    if record is not None:
+        return record.keyword
+    else:
+        return None
+
+
+def update_keyword(company, keyword_list):
+    keyword_json = json.dumps(keyword_list)
+    session.query(Keyword).filter(Keyword.company == company).update(
+        {Keyword.keyword: keyword_json}
+    )
+
+
+def delete_keyword(company):
+    session.query(Keyword).filter(Keyword.company == company).delete()
+    session.commit()
+
+
+# MySQL에서 먼저 조회를 시도하고 없으면 스크래핑을 시도
+def search_keyword(company):
+    try:
+        keyword_list = select_keyword(company)
+        if keyword_list == None:
+            print("[Debug] DB에 없어 스크래핑을 시작합니다.")
+            keyword_list = []
+            text_scraper = TextScraper()
+            keyword_extractor = KeywordExtractor()
+            href = text_scraper.get_href(company)
+            if href == None:
+                raise HTTPException(
+                    status_code=500, detail="회사명을 다시 입력해 주세요."
+                )
+            text = text_scraper.get_text(href)
+            if text == None:
+                raise HTTPException(
+                    status_code=500, detail="텍스트를 가져오는 데 실패했습니다."
+                )
+            nouns = keyword_extractor.extract_nouns(text)
+            if nouns == None:
+                raise HTTPException(status_code=500, detail="명사 추출에 실패했습니다.")
+            related_keywords = keyword_extractor.extract_related_keywords(nouns)
+            keyword_list = list(related_keywords)
+            insert_keyword(company, keyword_list)
+        return keyword_list
+    except Exception as e:
+        print(e)
+    finally:
+        # driver.quit()
+        pass
+
+
+# 인재상 검색 함수를 병렬로 처리하면서 15초 이상 걸리면 408 Request Timeout 에러를 반환
+def thread_search_keyword(company):
+    # @see https://docs.python.org/ko/3/library/concurrent.futures.html
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future = executor.submit(search_keyword, company)
+        try:
+            return future.result(timeout=15)
+        except concurrent.futures.TimeoutError:
+            raise HTTPException(status_code=408, detail="Request Timeout")

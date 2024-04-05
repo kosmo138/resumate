714586533c1ef816fee37e8d45e1068d6a4f88c0
suyons <su02ga@outlook.com>
2024-04-05 16:38:20 +0900

    Fix: ModuleNotFoundError 해결 (#59)
    2024-04-05 16:38
    - run.sh: EXPORT PYTHONPATH 추가
    - requirements.txt: langchain 추가
    - run_openai.py: langchain을 이용한 OpenAI API 호출 코드 추가
    - 예상한대로 응답 텍스트가 출력되지는 않아 수정 필요

---
+++ data/app/service/run_openai.py
@@ -0,0 +1,16 @@
+from langchain.chains import LLMChain
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAI
+from app.core.env import OPENAI_API_KEY
+
+# See: https://python.langchain.com/docs/integrations/llms/openai/
+if __name__ == "__main__":
+    template = """Question: {question}
+    Answer: Let's think step by step."""
+    prompt = PromptTemplate.from_template(template)
+
+    llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    question = "What NFL team won the Super Bowl in the year Justin Beiber was born?"
+
+    llm_chain.run(question)

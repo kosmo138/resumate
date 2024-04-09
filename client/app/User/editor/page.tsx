d0e876b67bf34b5dff3b78dee214cca6ec9fc3ba
mogri89 <tago8911@naver.com>
2024-04-09 18:07:16 +0900

    test: 이력서 페이지 구현 기능 검증 및 fetch 함수 error 발생 시 대응 방안 보완

--- client/app/User/editor/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function edit() {
-  return <h1>이력서</h1>;
-}
-// UI는 POST와 동일
-// 버튼  : 저장->/User/Resume/View
--- client/app/User/help/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function view() {
-  return <h1>View</h1>;
-}
-// 버튼: 수정

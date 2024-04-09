37105b2870854381a4926d947c9305b3be6028d1
mogri89 <151429200+mogri89@users.noreply.github.com>
2024-04-09 10:18:21 +0900

    Merge PR #71 from 'feat/resume-1'
    2024-04-09 10:19

--- client/components/letter/lettercard.tsx
+++ client/components/letter/lettercard.tsx
@@ -1,11 +1,14 @@
 import { Card, CardContent, CardFooter } from "@/components/ui/card"
 import { LetterHead } from "@/types/letter"
+import Link from "next/link"
 
 export default function LetterCard({ letterHead }: { letterHead: LetterHead }) {
   return (
     <Card>
       <CardContent>
-        <h3>{letterHead.title}</h3>
+        <Link href={`/letter/${letterHead.id}`}>
+          <h3>{letterHead.title}</h3>
+        </Link>
       </CardContent>
       <CardFooter>
         <p>{letterHead.modified}</p>

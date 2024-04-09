affbbcf0d249c0a951aa7f49651d9c094a7380c5
mogri89 <tago8911@naver.com>
2024-04-09 10:08:59 +0900

    fix: 서버 파일 오류 수동으로 수정

--- data/app/core/database.py
+++ data/app/core/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from core.env import DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
+from app.core.env import DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME
 
 DB_URI = f"mysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(DB_URI)

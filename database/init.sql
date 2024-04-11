371573b23362cdccb5846a8cba3a206fa0a7f9b2
Sooyoung <github.choice044@passmail.com>
2024-04-11 10:37:41 +0900

    Merge PR #75 from 'feat/prototype'
    2024-04-11 10:37
    - (client) 불필요 코드 삭제 (app/User)
    - (server) 자기소개서 CRUD REST API 작성
    - (server) 카카오 로그인 로직 구현
    - (server) 기존 코드 일부 리팩토링

--- database/init.sql
+++ database/init.sql
@@ -1,9 +1,18 @@
 /* root 계정으로 접속 */
+/* root 계정으로 접속 */
 SHOW DATABASES;
 
 ALTER USER 'root' undefined IDENTIFIED BY '{MYSQL_ROOTPW}';
 
+
+ALTER USER 'root' undefined IDENTIFIED BY '{MYSQL_ROOTPW}';
+
 CREATE DATABASE resumate;
+
+CREATE USER '{MYSQL_USERNAME}' undefined IDENTIFIED BY '{MYSQL_password}';
+
+GRANT ALL PRIVILEGES ON resumate.* TO '{MYSQL_USERNAME}' undefined;
+
 
 CREATE USER '{MYSQL_USERNAME}' undefined IDENTIFIED BY '{MYSQL_password}';
 
@@ -15,6 +24,10 @@ FLUSH PRIVILEGES;
 DROP TABLE IF EXISTS member;
 
 DROP TABLE IF EXISTS resume;
+
+DROP TABLE IF EXISTS letter;
+
+DROP TABLE IF EXISTS keyword;
 
 /* 테이블 생성 */
 CREATE TABLE member (
@@ -32,6 +45,25 @@ CREATE TABLE resume (
     FOREIGN KEY (email) REFERENCES member(email) ON DELETE CASCADE
 );
 
+CREATE TABLE letter (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(30) NOT NULL,
+    resume_id INT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    company VARCHAR(100) NOT NULL,
+    job VARCHAR(100) NOT NULL,
+    content JSON NOT NULL,
+    modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (email) REFERENCES member(email) ON DELETE CASCADE,
+    FOREIGN KEY (resume_id) REFERENCES resume(id) ON DELETE CASCADE
+);
+
+CREATE TABLE keyword (
+    id SMALLINT AUTO_INCREMENT PRIMARY KEY,
+    company VARCHAR(100) NOT NULL UNIQUE,
+    keyword JSON NOT NULL
+);
+
 /* 테이블 조회 */
 SHOW TABLES;
 
@@ -39,10 +71,18 @@ DESCRIBE member;
 
 DESCRIBE resume;
 
+DESCRIBE letter;
+
+DESCRIBE keyword;
+
 /* 테이블 설정 변경 */
 ALTER TABLE member AUTO_INCREMENT = 1;
 
 ALTER TABLE resume AUTO_INCREMENT = 1;
+
+ALTER TABLE letter AUTO_INCREMENT = 1;
+
+ALTER TABLE keyword AUTO_INCREMENT = 1;
 
 ALTER TABLE resume
 MODIFY COLUMN modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
@@ -76,7 +116,6 @@ SELECT id,
     UNIX_TIMESTAMP(modified) AS modified
 FROM resume
 WHERE email = #{email} ORDER BY modified DESC
-
 UPDATE resume
 SET title = 'test resume 02',
     content = '{"message":"hello"}',
@@ -85,3 +124,45 @@ WHERE id = 1;
 
 DELETE FROM resume
 WHERE id = 1;
+
+/* letter CRUD */
+INSERT INTO letter (email, resume_id, title, company, job, content)
+VALUES (
+        '1@test.com',
+        1,
+        'test letter 01',
+        'test company',
+        'test job',
+        '{"category": "지원동기", "content": "지원동기 내용입니다."}'
+    );
+    
+SELECT id,
+    title,
+    UNIX_TIMESTAMP(modified) AS modified
+FROM letter
+WHERE email = '1@test.com'
+ORDER BY modified DESC;
+
+UPDATE letter
+SET title = 'test letter 02',
+    company = 'test company 02',
+    job = 'test job 02',
+    content = '{"category": "지원동기", "content": "수정된 지원동기 내용입니다."}',
+    modified = NOW()
+WHERE id = 1;
+
+DELETE FROM letter
+WHERE id = 1;
+
+/* keyword CRUD */
+INSERT INTO keyword (company, keyword)
+VALUES ('abc', '{"a", "b", "c"}');
+
+SELECT id, company, keyword FROM keyword;
+
+UPDATE keyword
+SET keyword = '{"a", "b", "c", "d"}'
+WHERE company = 'abc';
+
+DELETE FROM keyword
+WHERE company = 'abc';
---
+++ server/src/main/java/resumate/server/service/KakaoAuthService.java
@@ -0,0 +1,76 @@
+package resumate.server.service;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import resumate.server.config.JwtConfig;
+import resumate.server.config.SecurityConfig;
+import resumate.server.dto.Member;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoAuthService {
+    private final MemberService memberService;
+    private final JwtConfig jwtConfig;
+    private final SecurityConfig securityConfig;
+
+    @Value("${kakao.apikey}")
+    private String kakaoApiKey;
+
+    @Value("${kakao.redirect_uri}")
+    private String redirectUri;
+
+    public RedirectView kakaoAuth(String code, HttpServletResponse response) {
+        final String tokenJson = getTokenJson(code);
+        final String accessToken = getIdTokenFromJson(tokenJson);
+        final String email = jwtConfig.getEmailFromToken(accessToken);
+        /* checkMemberMail: 신규 회원이라면 true, 기존 회원이라면 false */
+        if (memberService.checkMemberMail(email)) {
+            registerKakaoMember(email);
+        }
+        Cookie cookie = new Cookie("authorization", accessToken);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return new RedirectView("http://localhost/");
+    }
+
+    private void registerKakaoMember(String email) {
+        final String now = Long.toString(new Date().getTime());
+        final String rawPassword = "kakao_" + email + now;
+        final String password = securityConfig.passwordEncoder().encode(rawPassword);
+        Member member = new Member();
+        member.setEmail(email);
+        member.setPassword(password);
+        memberService.insertMember(member);
+    }
+
+    private String getTokenJson(String code) {
+        final String requestBody = "grant_type=authorization_code" + "&client_id=" + kakaoApiKey
+                + "&redirect_uri=" + redirectUri + "&code=" + code;
+        final WebClient client = WebClient.create();
+        final String tokenJson = client.post()
+                .uri("https://kauth.kakao.com/oauth/token")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                .bodyValue(requestBody)
+                .retrieve().bodyToMono(String.class).block();
+        return tokenJson;
+    }
+
+    private String getIdTokenFromJson(String json) {
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(json, JsonObject.class);
+        return jsonObject.get("id_token").getAsString();
+    }
+}

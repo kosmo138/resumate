84222edca613d1010ee71161a80f1e5525715bd4
suyons <github.choice044@passmail.com>
2024-04-12 16:46:16 +0900

    Docs: Q&A 페이지 컨텐츠 추가
    2024-04-12 16:46
    @taeyounh의 커밋 dd4c1ca 내용을 그대로 가져오기

--- client/config/qna-content.ts
+++ client/config/qna-content.ts
@@ -1,53 +1,38 @@
 export const articles = [
   {
     id: "0",
-    subject: "Meeting Tomorrow",
-    text: "Hi, let's have a meeting tomorrow to discuss the project. I've been reviewing the project details and have some ideas I'd like to share. It's crucial that we align on our next steps to ensure the project's success.\n\nPlease come prepared with any questions or insights you may have. Looking forward to our meeting!\n\nBest regards, William",
+    subject: "Resumate를 소개합니다.",
+    text: "Resumate는 최소한의 사용자 정보 입력으로 AI가 자동으로 최적의 이력서, 자기소개서를 작성해줍니다. ",
   },
   {
     id: "1",
-    subject: "Re: Project Update",
-    text: "Thank you for the project update. It looks great! I've gone through the report, and the progress is impressive. The team has done a fantastic job, and I appreciate the hard work everyone has put in.\n\nI have a few minor suggestions that I'll include in the attached document.\n\nLet's discuss these during our next meeting. Keep up the excellent work!\n\nBest regards, Alice",
+    subject: "이력서 작성 안내",
+    text: "이력서 작성 페이지에서 이력서 생성에 필요한 정보를 입력 한 후 작성 버튼을 누르면 연결 된 AI가 자동으로 문장을 추천 해줍니다.",
   },
   {
     id: "2",
-    subject: "Weekend Plans",
-    text: "Any plans for the weekend? I was thinking of going hiking in the nearby mountains. It's been a while since we had some outdoor fun.\n\nIf you're interested, let me know, and we can plan the details. It'll be a great way to unwind and enjoy nature.\n\nLooking forward to your response!\n\nBest, Bob",
+    subject: "자기소개서 작성 안내",
+    text: "이력서를 작성한 후 자기소개서 작성 페이지에서 자기소개서 생성에 필요한 정보를 입력 한 후 작성 버튼을 누르면 작성 된 이력서를 바탕으로 연결 된 AI가 자동으로 문장을 추천 해줍니다.",
   },
   {
     id: "3",
-    subject: "Re: Question about Budget",
-    text: "I have a question about the budget for the upcoming project. It seems like there's a discrepancy in the allocation of resources.\n\nI've reviewed the budget report and identified a few areas where we might be able to optimize our spending without compromising the project's quality.\n\nI've attached a detailed analysis for your reference. Let's discuss this further in our next meeting.\n\nThanks, Emily",
+    subject: "다크모드 관련 안내",
+    text: "다크모드 설정은 메인화면 우측 상단 버튼을 클릭하시면 라이트, 다크, 시스템 설정모드로 조절이 가능합니다.",
   },
   {
     id: "4",
-    subject: "Important Announcement",
-    text: "I have an important announcement to make during our team meeting. It pertains to a strategic shift in our approach to the upcoming product launch. We've received valuable feedback from our beta testers, and I believe it's time to make some adjustments to better meet our customers' needs.\n\nThis change is crucial to our success, and I look forward to discussing it with the team. Please be prepared to share your insights during the meeting.\n\nRegards, Michael",
+    subject: "이력서 작성 Tip",
+    text: "본인의 업무 경험을 바탕으로 핵심 역량과 업무 스킬을 작성해주세요. 신입 지원의 경우 직무와 관련된 대외활동, 인턴, 계약직 경력등이 있다면 작성해주세요.",
   },
   {
     id: "5",
-    subject: "Re: Feedback on Proposal",
-    text: "Thank you for your feedback on the proposal. It looks great! I'm pleased to hear that you found it promising. The team worked diligently to address all the key points you raised, and I believe we now have a strong foundation for the project.\n\nI've attached the revised proposal for your review.\n\nPlease let me know if you have any further comments or suggestions. Looking forward to your response.\n\nBest regards, Sarah",
+    subject: "졸업년도 계산기 사용법",
+    text: "사람인 홈페이지에 접속한 후에 취업 TOOL 탭에 존재하는 졸업/전역년도 계산기를 사용하면 손쉽게 검색할 수 있습니다.",
   },
   {
     id: "6",
-    subject: "New Project Idea",
-    text: "I have an exciting new project idea to discuss with you. It involves expanding our services to target a niche market that has shown considerable growth in recent months.\n\nI've prepared a detailed proposal outlining the potential benefits and the strategy for execution.\n\nThis project has the potential to significantly impact our business positively. Let's set up a meeting to dive into the details and determine if it aligns with our current goals.\n\nBest regards, David",
-  },
-  {
-    id: "7",
-    subject: "Vacation Plans",
-    text: "Let's plan our vacation for next month. What do you think? I've been thinking of visiting a tropical paradise, and I've put together some destination options.\n\nI believe it's time for us to unwind and recharge. Please take a look at the options and let me know your preferences.\n\nWe can start making arrangements to ensure a smooth and enjoyable trip.\n\nExcited to hear your thoughts! Olivia",
-  },
-  {
-    id: "8",
-    subject: "Re: Conference Registration",
-    text: "I've completed the registration for the conference next month. The event promises to be a great networking opportunity, and I'm looking forward to attending the various sessions and connecting with industry experts.\n\nI've also attached the conference schedule for your reference.\n\nIf there are any specific topics or sessions you'd like me to explore, please let me know. It's an exciting event, and I'll make the most of it.\n\nBest regards, James",
-  },
-  {
-    id: "9",
-    subject: "Team Dinner",
-    text: "Let's have a team dinner next week to celebrate our success. We've achieved some significant milestones, and it's time to acknowledge our hard work and dedication.\n\nI've made reservations at a lovely restaurant, and I'm sure it'll be an enjoyable evening.\n\nPlease confirm your availability and any dietary preferences. Looking forward to a fun and memorable dinner with the team!\n\nBest, Sophia",
+    subject: "국가 공인 자격증 검색 방법",
+    text: "사용자가 소지한 국가기술자격증을 조회할 때 한국산업인력공단 홈페이지에서 검색 가능합니다.",
   },
 ]
 

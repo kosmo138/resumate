bfa501a26ee07996dd6bbd9564d592f0b39fa1d3
taeyounh <dbs531900@gmail.com>
2024-04-12 16:11:30 +0900

    FAQ 팁, 질문 작성

--- client/components/qna/data.tsx
+++ client/components/qna/data.tsx
@@ -1,95 +1,39 @@
-export const mails = [
+export const articles = [
   {
-    id: "6c84fb90-12c4-11e1-840d-7b25c5ee775a",
-    name: "Resumate",
-    email: "resumate@example.com",
+    id: "0",
     subject: "Resumate를 소개합니다.",
-    text: "저희 Resumate는 AI를 이용하여 빠른 시간내에 원하는 이력서, 자기소개서를 작성해줄 수 있습니다.",
-    date: "2024-04-11T09:00:00",
-    read: true,
-    labels: ["서비스 소개"],
+    text: "Resumate는 최소한의 사용자 정보 입력으로 AI가 자동으로 최적의 이력서, 자기소개서를 작성해줍니다. ",
   },
   {
-    id: "110e8400-e29b-11d4-a716-446655440000",
-    name: "Resumate",
-    email: "resumate@example.com",
-    subject: "이력서 작성방법 안내",
-    text: "이력서 작성 화면에 표시 된 정보를 입력하신 후 작성 버튼을 클릭하시면 AI가 자동으로 문장을 추천 해줍니다.",
-    date: "2024-04-05T10:30:00",
-    read: true,
-    labels: ["이력서"],
+    id: "1",
+    subject: "이력서 작성 안내",
+    text: "이력서 작성 페이지에서 이력서 생성에 필요한 정보를 입력 한 후 작성 버튼을 누르면 연결 된 AI가 자동으로 문장을 추천 해줍니다.",
   },
   {
-    id: "3e7c3f6d-bdf5-46ae-8d90-171300f27ae2",
-    name: "Resumate",
-    email: "resumate@example.com",
-    subject: "자기소개서 작성방법 안내",
-    text: "자기소개서 작성 화면에 표시 된 정보를 입력하신 후 작성 버튼을 클릭하시면 AI가 자동으로 문장을 작성 해줍니다",
-    date: "2023-04-10T11:45:00",
-    read: true,
-    labels: ["자기소개서"],
+    id: "2",
+    subject: "자기소개서 작성 안내",
+    text: "이력서를 작성한 후 자기소개서 작성 페이지에서 자기소개서 생성에 필요한 정보를 입력 한 후 작성 버튼을 누르면 작성 된 이력서를 바탕으로 연결 된 AI가 자동으로 문장을 추천 해줍니다.",
   },
   {
-    id: "61c35085-72d7-42b4-8d62-738f700d4b92",
-    name: "Resumate",
-    email: "resumate@example.com",
-    subject: "FAQ 게시판 사용안내",
-    text: "궁금하신 질문을 남겨주시면 빠른 시일내에 답변 드리도록 하겠습니다.",
-    date: "2023-04-2T13:15:00",
-    read: false,
-    labels: ["이용안내"],
+    id: "3",
+    subject: "다크모드 관련 안내",
+    text: "다크모드 설정은 메인화면 우측 상단 버튼을 클릭하시면 라이트, 다크, 시스템 설정모드로 조절이 가능합니다.",
+  },
+  {
+    id: "4",
+    subject: "이력서 작성 Tip",
+    text: "본인의 업무 경험을 바탕으로 핵심 역량과 업무 스킬을 작성해주세요. 신입 지원의 경우 직무와 관련된 대외활동, 인턴, 계약직 경력등이 있다면 작성해주세요.",
+  },
+  {
+    id: "5",
+    subject: "졸업년도 계산기 사용법",
+    text:"사람인 홈페이지에 접속한 후에 취업 TOOL 탭에 존재하는 졸업/전역년도 계산기를 사용하면 손쉽게 검색 할 수 있습니다.  ",
+  },
+  {
+    id: "6",
+    subject: "국가 공인 자격증 검색 방법",
+    text: "사용자가 소지한 국가기술자격증을 조회 할 때 한국산업인력공단 홈페이지에서 검색 가능합니다.",
   }
 ]
 
-export type Mail = (typeof mails)[number]
-
-export const accounts = [
-  {
-    label: "Alicia Koch",
-    email: "alicia@example.com",
-    icon: (
-      <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <title>Vercel</title>
-        <path d="M24 22.525H0l12-21.05 12 21.05z" fill="currentColor" />
-      </svg>
-    ),
-  },
-  {
-    label: "Alicia Koch", 
-    email: "alicia@gmail.com",
-    icon: (
-      <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <title>Gmail</title>
-        <path
-          d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"
-          fill="currentColor"
-        />
-      </svg>
-    ),
-  },
-  {
-    label: "Alicia Koch",
-    email: "alicia@me.com",
-    icon: (
-      <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <title>iCloud</title>
-        <path
-          d="M13.762 4.29a6.51 6.51 0 0 0-5.669 3.332 3.571 3.571 0 0 0-1.558-.36 3.571 3.571 0 0 0-3.516 3A4.918 4.918 0 0 0 0 14.796a4.918 4.918 0 0 0 4.92 4.914 4.93 4.93 0 0 0 .617-.045h14.42c2.305-.272 4.041-2.258 4.043-4.589v-.009a4.594 4.594 0 0 0-3.727-4.508 6.51 6.51 0 0 0-6.511-6.27z"
-          fill="currentColor"
-        />
-      </svg>
-    ),
-  },
-]
-
-export type Account = (typeof accounts)[number]
-
-export const contacts = [
-  {
-    name: "Resumate",
-    email: "resumate.resumate@example.com",
-  },
-  
-]
-
-export type Contact = (typeof contacts)[number]
+export type Article = (typeof articles)[number]

c0bfb573f860892f0c44af95953526186e011e26
suyons <su02ga@outlook.com>
2024-04-13 17:45:29 +0900

    Fix: GET /api/resume 요청 시 오류 수정 (#85)
    2024-04-13 17:45
    data/app/api/routes/letter.py:
      - 요청 URL 변경: localhost -> host.docker.internal
      - import 구문 수정
      - json 형식으로 반환하도록 수정
    
    data/run.sh:
      - uvicorn: not found 오류 수정
    
    Close #85

--- data/app/api/routes/letter.py
+++ data/app/api/routes/letter.py
@@ -1,13 +1,11 @@
 from fastapi import APIRouter, Header, Body, Response
 from app.service.letterservice import LetterService
 from app.core.models import Letter
-
-
 router = APIRouter()
 letter_service = LetterService()
 
 
 @router.post("/")
-async def post_letter(authorization: str = Header(None), letter: Letter = Body(...)):
-    result: str = await letter_service.post_letter(authorization, letter)
-    return Response(content=result, media_type="application/json")
+def post_letter(authorization: str = Header(None), letter: Letter = Body(...)):
+    result_json = letter_service.post_letter(authorization, letter)
+    return Response(content=result_json, media_type="application/json")

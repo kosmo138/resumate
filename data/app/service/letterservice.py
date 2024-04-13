cc6fff139e2e79581b90b2aa71b3300a96fcf441
suyons <github.choice044@passmail.com>
2024-04-13 16:57:01 +0900

    Refactor: requests 대신 urllib 사용 (#85)
    2024-04-13 16:57
    data/app/service/letterservice.py:
      - requests.get 대신 urllib.request.urlopen 사용

--- data/app/service/letterservice.py
+++ data/app/service/letterservice.py
@@ -1,4 +1,4 @@
-import requests
+from urllib import request
 import concurrent.futures
 from fastapi import HTTPException
 from app.core.models import Letter
@@ -13,19 +13,17 @@ class LetterService:
                 return future.result(timeout=30)
             except concurrent.futures.TimeoutError:
                 raise HTTPException(status_code=408, detail="요청 시간 초과")
-    def get_resume_by_id(authorization: str, letter: Letter) -> str:
+
+    def get_resume_by_id(self, authorization: str, letter: Letter) -> str:
         if authorization is None:
-                raise HTTPException(status_code=401, detail="로그인이 필요합니다")
-        try:
-            headers = {"Authorization": authorization}
-            response = requests.get(
-                f"http://localhost/api/resume/{letter.resume_id}", headers=headers
-            )
-            if response.status_code == 200:
-                return response.json()
+            raise HTTPException(status_code=401, detail="로그인이 필요합니다")
+        url: str = f"http://localhost/api/resume/{letter.resume_id}"
+        headers: dict = {"Authorization": authorization}
+        req = request.Request(url=url, headers=headers)
+        with request.urlopen(req) as response:
+            if response.status == 200:
+                return response.read().decode("utf-8")
             else:
                 raise HTTPException(
-                    status_code=response.status_code, detail="이력서 정보 조회 실패"
+                    status_code=response.status, detail="이력서 정보 조회 실패"
                 )
-        except ConnectionError as e:
-            print(e)

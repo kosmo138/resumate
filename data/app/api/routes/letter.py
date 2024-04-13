1033fb784343849a8041edc3bedb84cf036ec4c7
suyons <github.choice044@passmail.com>
2024-04-13 19:36:24 +0900

    Refactor: LetterService 클래스 구조 변경 (#67)
    2024-04-13 19:36
    data/app/service/letterservice.py:
      - 비즈니스 로직을 최소 단위로 분리하고, 병합하기 위해 main() 메서드 추가

--- data/app/api/routes/letter.py
+++ data/app/api/routes/letter.py
@@ -1,3 +1,4 @@
+import json
 from fastapi import APIRouter, Header, Body, Response
 from app.service.letterservice import LetterService
 from app.core.models import Letter
@@ -7,5 +8,5 @@ letter_service = LetterService()
 
 @router.post("/")
 def post_letter(authorization: str = Header(None), letter: Letter = Body(...)):
-    result_json = letter_service.post_letter(authorization, letter)
-    return Response(content=result_json, media_type="application/json")
+    result: str = letter_service.thread_main(authorization, letter)
+    return Response(content=result, media_type="application/json")
--- data/app/service/letterservice.py
+++ data/app/service/letterservice.py
@@ -6,21 +6,27 @@ from app.core.models import Resume, Letter
 
 
 class LetterService:
-    def post_letter(self, authorization: str, letter: Letter) -> str:
+    def main(self, authorization: str, letter: Letter):
+        resume = self.get_resume_by_id(authorization, letter.resume_id)
+        resume_json = json.dumps(resume)
+        return resume_json
+
+    def thread_main(self, authorization: str, letter: Letter) -> str:
         with ThreadPoolExecutor() as executor:
             try:
-                future = executor.submit(self.get_resume_by_id, authorization, letter)
-                result = future.result(timeout=30)
-                return json.dumps(result)
+                future = executor.submit(self.main, authorization, letter)
+                return future.result(timeout=30)
             except TimeoutError:
                 raise HTTPException(
                     status_code=408, detail="요청 시간이 초과되었습니다"
                 )
+            except Exception:
+                raise HTTPException(status_code=500, detail="서버 오류가 발생했습니다")
 
-    def get_resume_by_id(self, authorization: str, letter: Letter) -> str:
-        if authorization is None:
+    def get_resume_by_id(self, authorization: str, resume_id: int) -> Resume:
+        if len(authorization) == 0 or authorization is None:
             raise HTTPException(status_code=401, detail="로그인이 필요합니다")
-        url: str = f"http://host.docker.internal/api/resume/{letter.resume_id}"
+        url: str = f"http://host.docker.internal/api/resume/{resume_id}"
         headers: dict = {"Authorization": authorization}
         response = requests.get(url=url, headers=headers)
         if response.status_code == 200:
@@ -30,6 +36,3 @@ class LetterService:
                 status_code=response.status_code,
                 detail="이력서 정보 가져오기에 실패했습니다",
             )
-
-    def openai_draft(self, resume: Resume, letter: Letter):
-        pass

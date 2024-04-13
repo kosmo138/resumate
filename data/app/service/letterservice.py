42745f89a380c9022ead5b2b933632d3b3fdf5b4
suyons <su02ga@outlook.com>
2024-04-14 01:10:43 +0900

    Refactor: 토큰을 생성자에서 주입받도록 구조 변경 (#67)
    2024-04-14 01:08
    data/app/api/routes/letter.py:
      - 생성자 매개변수로 토큰 전달
    
    data/app/service/letterservice.py:
      - 생성자에서 토큰 주입받도록 변경
      - main 함수에서 인재상 키워드 검색 기능 확인 코드 추가

--- data/app/service/letterservice.py
+++ data/app/service/letterservice.py
@@ -3,18 +3,23 @@ import requests
 from concurrent.futures import ThreadPoolExecutor, TimeoutError
 from fastapi import HTTPException
 from app.core.models import Resume, Letter
+from app.service.keywordservice import KeywordService
 
 
 class LetterService:
-    def main(self, authorization: str, letter: Letter):
-        resume = self.get_resume_by_id(authorization, letter.resume_id)
-        resume_json = json.dumps(resume)
-        return resume_json
+    def __init__(self):
+        self.keyword_service = KeywordService()
+        self.authorization = ""
 
-    def thread_main(self, authorization: str, letter: Letter) -> str:
+    def main(self, letter: Letter) -> str:
+        # resume = self.get_resume_by_id(self.authorization, letter.resume_id)
+        company_keyword = self.keyword_service.search_keyword(letter.company)
+        return company_keyword
+
+    def thread_main(self, letter: Letter) -> str:
         with ThreadPoolExecutor() as executor:
             try:
-                future = executor.submit(self.main, authorization, letter)
+                future = executor.submit(self.main, self.authorization, letter)
                 return future.result(timeout=30)
             except TimeoutError:
                 raise HTTPException(
@@ -22,12 +27,15 @@ class LetterService:
                 )
             except Exception:
                 raise HTTPException(status_code=500, detail="서버 오류가 발생했습니다")
+            finally:
+                executor.shutdown(wait=False)
+                del self
 
-    def get_resume_by_id(self, authorization: str, resume_id: int) -> Resume:
-        if len(authorization) == 0 or authorization is None:
+    def get_resume_by_id(self, resume_id: int) -> Resume:
+        if self.authorization == "":
             raise HTTPException(status_code=401, detail="로그인이 필요합니다")
         url: str = f"http://host.docker.internal/api/resume/{resume_id}"
-        headers: dict = {"Authorization": authorization}
+        headers: dict = {"Authorization": self.authorization}
         response = requests.get(url=url, headers=headers)
         if response.status_code == 200:
             return response.json()
@@ -36,3 +44,4 @@ class LetterService:
                 status_code=response.status_code,
                 detail="이력서 정보 가져오기에 실패했습니다",
             )
+

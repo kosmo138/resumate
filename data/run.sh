8f883946a0f81fd9a3f81415c364747350809d7c
suyons <su02ga@outlook.com>
2024-04-13 11:17:27 +0900

    Chore: Docker 이미지 변경 (#84)
    2024-04-13 11:23
    docker-compose.yml:
      - Docker에서 요구하는 자원을 줄이기 위해 Alpine Linux 기반 이미지로 변경
      - 의존성을 저장하는 디렉토리를 컨테이너 내부가 아닌 볼륨으로 변경
    requirements.txt:
      - 불필요 의존성 제거: torch, transformers
    data/installer:
      - 필요 패키지를 설치하는 스크립트를 별도의 폴더로 이동
      - chromedriver, pip, scipy 설치 스크립트 추가
    data/timesleep.py:
      - docker exec -it data /bin/sh 명령어로 컨테이너에 접속하기 위해 아무 일 하지 않는 스크립트 추가
    server/run.sh:
      - server 컨테이너의 기반 이미지 변경으로 인한 명령어 수정
    data/run.sh:
      - data 컨테이너 접속을 위한 timesleep.py 실행 명령어 추가
    .girignore:
      - Maven 및 pip가 다운로드하는 의존성 파일을 무시하도록 추가

--- data/run.sh
+++ data/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-export PYTHONPATH=$PWD
-pip install --no-cache-dir --upgrade -r ./requirements.txt
-uvicorn app.main:app --host "0.0.0.0" --port 8000 --reload
+# export PYTHONPATH=$PWD
+# pip install --no-cache-dir --upgrade -r ./requirements.txt
+# uvicorn app.main:app --host "0.0.0.0" --port 8000 --reload
+python timesleep.py

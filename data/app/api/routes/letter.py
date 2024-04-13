8ee605dd2ea4f9a5f2461d0eb20103341e9a3365
suyons <github.choice044@passmail.com>
2024-04-14 01:10:43 +0900

    Refactor: 토큰을 생성자에서 주입받도록 구조 변경 (#67)
    2024-04-14 01:08
    data/app/api/routes/letter.py:
      - 생성자 매개변수로 토큰 전달
    
    data/app/service/letterservice.py:
      - 생성자에서 토큰 주입받도록 변경
      - main 함수에서 인재상 키워드 검색 기능 확인 코드 추가

--- data/app/api/routes/letter.py
+++ data/app/api/routes/letter.py
@@ -2,11 +2,12 @@ import json
 from fastapi import APIRouter, Header, Body, Response
 from app.service.letterservice import LetterService
 from app.core.models import Letter
+
 router = APIRouter()
-letter_service = LetterService()
 
 
 @router.post("/")
 def post_letter(authorization: str = Header(None), letter: Letter = Body(...)):
+    letter_service = LetterService(authorization=authorization)
     result: str = letter_service.thread_main(authorization, letter)
     return Response(content=result, media_type="application/json")

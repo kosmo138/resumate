da5c54e5fde862825d376ac894dda5147a8f50ca
suyons <github.choice044@passmail.com>
2024-04-13 17:45:29 +0900

    Fix: GET /api/resume 요청 시 오류 수정 (#85)
    2024-04-13 17:45
    data/app/api/routes/letter.py:
      - 요청 URL 변경: localhost -> host.docker.internal
      - import 구문 수정
      - json 형식으로 반환하도록 수정
    
    data/run.sh:
      - uvicorn: not found 오류 수정
    
    Close #85

--- data/app/service/letterservice.py
+++ data/app/service/letterservice.py
@@ -1,5 +1,6 @@
-from urllib import request
-import concurrent.futures
+import json
+import requests
+from concurrent.futures import ThreadPoolExecutor, TimeoutError
 from fastapi import HTTPException
 from app.core.models import Letter
 
@@ -7,23 +8,26 @@ from app.core.models import Letter
 class LetterService:
     def post_letter(self, authorization: str, letter: Letter) -> str:
         args = (authorization, letter)
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            future = executor.submit(self.get_resume_by_id, *args)
+        with ThreadPoolExecutor() as executor:
             try:
-                return future.result(timeout=30)
-            except concurrent.futures.TimeoutError:
-                raise HTTPException(status_code=408, detail="요청 시간 초과")
+                future = executor.submit(self.get_resume_by_id, *args)
+                result = future.result(timeout=30)
+                return json.dumps(result)
+            except TimeoutError:
+                raise HTTPException(
+                    status_code=408, detail="요청 시간이 초과되었습니다"
+                )
 
     def get_resume_by_id(self, authorization: str, letter: Letter) -> str:
         if authorization is None:
             raise HTTPException(status_code=401, detail="로그인이 필요합니다")
-        url: str = f"http://localhost/api/resume/{letter.resume_id}"
+        url: str = f"http://host.docker.internal/api/resume/{letter.resume_id}"
         headers: dict = {"Authorization": authorization}
-        req = request.Request(url=url, headers=headers)
-        with request.urlopen(req) as response:
-            if response.status == 200:
-                return response.read().decode("utf-8")
-            else:
-                raise HTTPException(
-                    status_code=response.status, detail="이력서 정보 조회 실패"
-                )
+        response = requests.get(url=url, headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise HTTPException(
+                status_code=response.status_code,
+                detail="이력서 정보 가져오기에 실패했습니다",
+            )

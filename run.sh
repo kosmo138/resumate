1b76b58ce416e113c78411843ae44c24d8531076
suyons <github.choice044@passmail.com>
2024-04-15 22:08:16 +0900

    Fix: run.sh, navbar.tsx 수정 (#88)
    2024-04-15 22:08
    
    run.sh:
      - 경로 수정: ../ -> ./
    
    navbar.tsx:
      - 모바일 화면에서 로그인 메뉴가 보이지 않는 문제 수정

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd client && yarn run start &
-cd ../server && java -jar target/server-0.0.1.jar &
-cd ../data && uvicorn app.main:app --host "0.0.0.0" --port 8000 &
+cd ./server && java -jar target/server-0.0.1.jar &
+cd ./data && uvicorn app.main:app --host "0.0.0.0" --port 8000 &

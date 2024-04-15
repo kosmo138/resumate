9e0319e67b59a36e116add927ebfb8258d39658b
baeksona <100sona@naver.com>
2024-04-15 19:43:07 +0900

    Test: 프롬프트 템플릿 수정 테스트
    - requirements.txt : selenium 삭제

--- data/app/service/openai_prompt.py
+++ data/app/service/openai_prompt.py
@@ -14,19 +14,45 @@ class OpenaiPrompt:
         self.letter = letter
         self.keyword = keyword
 
-    def draft_letter(self):
-        resume_yaml: str = yaml.dump(self.resume, allow_unicode=True)
-        keyword_json: str = json.dumps(self.keyword[0:4], ensure_ascii=False)
-        template = (
-            "아래의 이력서를 통해 자기소개서 중 {category}의 초안을 500자 작성해 줘\n"
-            "\n"
-            "[회사명] {company}\n"
-            "[지원 직무] {job}\n"
-            "[포함할 키워드] {keyword}\n"
-            "\n"
-            "[이력서]\n"
-            "{resume}\n"
-        )
+    def draft_letter(self, text: str, command: str) -> str:
+        if not text.strip():  # 텍스트가 비어있는지 확인합니다
+            try:
+                resume_yaml: str = yaml.dump(self.resume, allow_unicode=True)
+                keyword_json: str = json.dumps(self.keyword[0:4], ensure_ascii=False)
+                template = (
+                    "아래의 이력서를 통해 자기소개서 중 {category}의 초안을 500자 작성해 줘\n"
+                    "\n"
+                    "[회사명] {company}\n"
+                    "[지원 직무] {job}\n"
+                    "[포함할 키워드] {keyword}\n"
+                    "\n"
+                    "[이력서]\n"
+                    "{resume}\n"
+                )
+                
+            except Exception:
+                raise HTTPException(
+                    status_code=500, detail="ChatGPT 요청 도중 오류가 발생했습니다"
+                )
+        else:
+        # 텍스트가 비어 있지 않은 경우에는 주어진 텍스트를 포함하여 자기소개서를 생성하는 템플릿을 설정합니다
+             resume_yaml: str = yaml.dump(self.resume, allow_unicode=True)
+             keyword_json: str = json.dumps(self.keyword[0:4], ensure_ascii=False)
+
+             template = (
+                "주어진 {text}를 바탕으로 {command}에 대한 내용을 반영해 수정해서 자기소개서를 생성하는 요청이야\n\n"
+                f"주어진 텍스트:\n{text}\n\n"
+                f"명령어:\n{command}\n\n"
+                "아래의 이력서를 통해 자기소개서를 작성해 줘\n"
+                "\n"
+                "[회사명] {company}\n"
+                "[지원 직무] {job}\n"
+                "[포함할 키워드] {keyword}\n"
+                "\n"
+                "[이력서]\n"
+                "{resume}\n"
+            )                
+                
         try:
             prompt_template = PromptTemplate(
                 template=template,
@@ -43,7 +69,7 @@ class OpenaiPrompt:
             )
             return result
 
-        except Exception:
+        except Exception as e:
             raise HTTPException(
                 status_code=500, detail="ChatGPT 요청 도중 오류가 발생했습니다"
             )

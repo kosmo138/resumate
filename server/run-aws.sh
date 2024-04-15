11b484165951181ecea7738e4e5e14b4099bbe83
suyons <github.choice044@passmail.com>
2024-04-15 13:07:53 +0900

    Chore: run-aws.sh 수정 (#88)
    2024-04-15 13:07
    
    run-aws.sh:
      - aws 서버 실행 스크립트 수정
    
    ResumeControllerTest.java:
      - 테스트 코드 삭제
    
    letterform.tsx & letter.d.ts:
      - 오류를 유발하는 자기소개서 기능 관련 코드 삭제

--- server/run-aws.sh
+++ server/run-aws.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-./mvnw spring-boot:run
+echo "[INFO] Building the package."
+sh mvnw clean package
+echo "[INFO] Starting Spring at port 8080."
+java -jar target/server-0.0.1.jar
--- server/run-dev.sh
+++ server/run-dev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sh mvnw spring-boot:run
+./mvnw spring-boot:run

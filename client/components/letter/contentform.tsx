0c07ad034052a2dee2c14eec8a2a5f85f5c4c317
mogri89 <tago8911@naver.com>
2024-04-17 15:48:49 +0900

    chore: letter페이지 기반이력서 기능, next.config.js rewirte 추가

--- client/components/letter/contentform.tsx
+++ client/components/letter/contentform.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { LetterBody, LetterContent } from "@/types/letter";
-import { useState } from "react";
+import { Input } from "../ui/input";
+import Cookies from "js-cookie";
 
 interface ContentFormProps {
   indexkey: number;
@@ -19,14 +20,33 @@ export default function ContentForm({
   content,
   letterBody,
   setLetterBody,
-  onRemove,
   onCategoryChange,
 }: ContentFormProps) {
-  const { category, text } = content;
+  const apiUrl = `/data/letter`;
+  const jwt = Cookies.get("authorization");
 
   const handleCategoryChange = (value: string) => {
-    console.log("2번째자식" + indexkey + value);
     onCategoryChange(indexkey, value); // 선택된 값을 부모 컴포넌트로 전달
+  };
+
+  const handleSave = () => {
+    console.log(letterBody);
+
+    fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${jwt}`,
+      },
+      body: JSON.stringify(letterBody), // 텍스트박스 내의 값 전체를 전송
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("저장에 실패했습니다.");
+        }
+        return response.json();
+      })
+      .catch(() => {});
   };
 
   return (
@@ -38,29 +58,45 @@ export default function ContentForm({
           </div>
           <CategorySelector
             value={content.category}
-            onValueChange={handleCategoryChange} // 인덱스와 카테고리를 전달
+            onValueChange={handleCategoryChange}
           />
-          <Button className="ml-4">생성</Button>
+          <Button onClick={handleSave} className="ml-4">
+            생성
+          </Button>
         </div>
       )}
-
       <div className="mb-4 ml-20 w-full">
-        <div className="items-center">
-          <Label className="text-1xl hidden">자기소개서 생성</Label>
-        </div>
         <div>
           <Textarea
             className="mt-2 w-2/3"
             value={content.text}
-            onChange={(e) => {
-              const newContent = letterBody.content.map((c, i) => {
-                if (i === indexkey) {
-                  return { ...c, text: e.target.value };
-                }
-                return c;
-              });
-              setLetterBody({ ...letterBody, content: newContent });
-            }}
+            onChange={(e) =>
+              setLetterBody({
+                ...letterBody,
+                content: [
+                  ...letterBody.content.slice(0, indexkey), // 이전 요소들을 그대로 유지
+                  { ...content, text: e.target.value }, // 현재 요소 업데이트
+                  ...letterBody.content.slice(indexkey + 1), // 나머지 요소들을 그대로 유지
+                ],
+              })
+            }
+          />
+        </div>
+        <div className="mt-3 mb-6 flex flex-row items-center">
+          <Input
+            className="w-2/5"
+            placeholder="추가 요구사항 입력"
+            value={content.command}
+            onChange={(e) =>
+              setLetterBody({
+                ...letterBody,
+                content: [
+                  ...letterBody.content.slice(0, indexkey), // 이전 요소들을 그대로 유지
+                  { ...content, command: e.target.value }, // 현재 요소 업데이트
+                  ...letterBody.content.slice(indexkey + 1), // 나머지 요소들을 그대로 유지
+                ],
+              })
+            }
           />
         </div>
       </div>

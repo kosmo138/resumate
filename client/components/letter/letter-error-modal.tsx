e1afadfcf0c0cf32f16e61517fb81579e3e660bd
mogri89 <tago8911@naver.com>
2024-04-18 12:29:10 +0900

    refactor: 쓸모없는 코드 삭제

--- client/components/letter/letter-error-modal.tsx
+++ client/components/letter/letter-error-modal.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import {
   Dialog,
   DialogContent,
@@ -10,7 +9,6 @@ import {
 } from "@/components/ui/dialog";
 import React, { useState, useEffect } from "react";
 
-//
 export default function LetterError({
   error,
   onConfirm,
@@ -20,8 +18,6 @@ export default function LetterError({
 }) {
   //dialog 상태 관리
   const [isOpen, setIsOpen] = useState(false);
-
-  const openDialog = () => setIsOpen(true);
   const closeDialog = () => setIsOpen(false);
 
   // 부모 컴포넌트에서 전달된 error props 값이 true일 때 isOpen 상태를 true로 변경
